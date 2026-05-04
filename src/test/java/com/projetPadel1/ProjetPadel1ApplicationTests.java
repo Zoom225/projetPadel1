8ff7da64c431f87550a02812b8e6e300fbafae39
mise-a-jour-backend <bobypro225@gmail.com>
2026-05-04 10:30:17 +0200

    initiation du projt padel

---
+++ src/test/java/com/projetPadel1/ProjetPadel1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetPadel1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetPadel1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
